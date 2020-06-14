973e3708ced7df802280afb3e0959f95eaab058c
amar <amarasagekar@gmail.com>
2020-06-14 15:06:29 +0530

    initial commit

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../constant.dart';
+
+class BottmButtom extends StatelessWidget {
+
+  BottmButtom({this.onTap, this.title});
+  final Function onTap;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(title,style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
